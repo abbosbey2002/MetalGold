b280eb904a7baa95787f713dd8857cfa96987955
Quvonchbek_Jalilov <=>
2024-05-28 10:06:54 +0500

    front page

--- lang/en/words.php
+++ lang/en/words.php
@@ -6,6 +6,8 @@ return
         'about' => 'About Us',
         'product' => 'Products',
         'contact' => 'Contact Us',
+        'category' => 'Categories',
+        'our' => 'Our',
         'blog' => 'Blog',
         'size' => 'Size',
         'cost' => 'Cost',
@@ -14,4 +16,13 @@ return
         'unit' => 'Unit',
         'back' => 'Back',
         'service' => 'Service',
+        'meetUp' => 'Meet Our Expert Team Members',
+        'clients' => "Partners",
+        'ourClients' => 'Our Partners',
+        'table' => 'Table',
+        'metal' => "Metal",
+        'company' => 'Company',
+        'read' => 'Read More',
+        'expStaff' => 'our Experienced Members',
+        'expMember' => 'We have many employees with many years of experience in the metal industry.We are proud of such experienced employees'
     ];
